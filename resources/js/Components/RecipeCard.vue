d2565a35cfb159fba97d4dc60dfa503478d8ac2c
Ayub <ayubmja@icloud.com>
2024-08-14 08:42:41 -0400

    build

--- resources/js/Components/RecipeCard.vue
+++ resources/js/Components/RecipeCard.vue
@@ -46,7 +46,7 @@ const handleBookmark = () => {
                 <div :class="compact ? 'flex flex-col justify-self-center': 'flex flex-col justify-self-center'">
                     <div :class="compact ? 'mx-auto' : 'm-auto'">
                         <img v-if="recipe.image_url" :src="recipe.image_url" alt="Recipe Image"
-                             :class="compact ? 'w-fit md:w-20 rounded-full':  'w-fit md:w-40  object-cover rounded-xl bg-orange-400 shadow-2xl dark:shadow-orange-300'">
+                             :class="compact ? 'w-fit md:w-20 rounded-full':  'w-fit md:w-40  object-cover rounded-xl bg-orange-400 shadow-2xl dark:shadow-orange-300 mb-8'">
                     </div>
                     <button v-if="!compact" class="bg-orange-500 text-white py-2 px-4 rounded-lg" @click="navigateToRecipe(recipe.id)">Info</button>
                 </div>
@@ -55,7 +55,8 @@ const handleBookmark = () => {
                         {{recipe.title}}
                     </div>
                     <div v-if="!compact" :class="compact ? 'container font-medium text-sm line-clamp-1': 'container font-medium text-lg line-clamp-2'">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+<!--                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.-->
+                        {{recipe.description}}
                     </div>
                     <div class="container  m-auto">
                         <button v-if="compact" class="bg-orange-500 text-white py-1 px-3 rounded-lg" @click="navigateToRecipe(recipe.id)">Info</button>
